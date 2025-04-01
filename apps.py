ee9b68290a528c760faaaa5a819a053c5f1714db
Afroze-07 <158141160+Afroze-07@users.noreply.github.com>
2025-04-01 16:28:19 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -3,7 +3,8 @@ import pandas as pd
 import plotly.express as px
 
 # Load dataset
-file_path = "D:\PROJECT\used_car_dataset.csv" # Ensure correct path
+file_path = r"D:\PROJECT\used_car_dataset.csv"
+ # Ensure correct path
 df = pd.read_csv(file_path)
 
 # Ensure consistent column names
