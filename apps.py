e858244e0f9ec3d1631a44c7dd140cff11f8e467
Syed Tasleem Afroze <158141160+Afroze-07@users.noreply.github.com>
2025-04-08 16:09:47 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -58,7 +58,7 @@ set_theme(theme)
 
 # --- Branding ---
 st.markdown("""
-    <h1 style='text-align: center;'>🚗 Used Car Insights Dashboard</h1>
+    <h1 style='text-align: center;' color:'white;'>🚗 Used Car Insights Dashboard</h1>
     <hr style='border-top: 3px solid #bbb;'>
 """, unsafe_allow_html=True)
 
